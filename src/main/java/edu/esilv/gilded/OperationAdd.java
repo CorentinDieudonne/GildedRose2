850f0cd28fbe9f1b5c1915cb29902679a97cd4e6
JANIW Karl-Joris <kariiwar@gmail.com>
2018-11-27 10:29:50 +0100

    push

--- src/main/java/edu/esilv/gilded/OperationAdd.java
+++ src/main/java/edu/esilv/gilded/OperationAdd.java
@@ -1,7 +1,6 @@
 package edu.esilv.gilded;
 
 public class OperationAdd {
-    @Override
     public int doOperation(int num1, int num2) {
         return num1 + num2;
     }
--- src/main/java/edu/esilv/gilded/OperationSubsctract.java
+++ src/main/java/edu/esilv/gilded/OperationSubsctract.java
@@ -1,7 +1,6 @@
 package edu.esilv.gilded;
 
 public class OperationSubsctract {
-    @Override
     public int doOperation(int num1, int num2) {
         return num1 - num2;
     }

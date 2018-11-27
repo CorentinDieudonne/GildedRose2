03a58c525a80772f0f9d63cac72dc5e747e1ef9d
JANIW Karl-Joris <kariiwar@gmail.com>
2018-11-27 17:09:27 +0100

    push

---
+++ src/main/java/edu/esilv/gilded/Concert.java
@@ -0,0 +1,8 @@
+package edu.esilv.gilded;
+
+public class Concert implements Strategy {
+    @Override
+    public void update(){
+        System.out.print("This is a Concert");
+    }
+}
---
+++ src/main/java/edu/esilv/gilded/Elixir.java
@@ -0,0 +1,8 @@
+package edu.esilv.gilded;
+
+public class Elixir implements Strategy {
+    @Override
+    public void update(){
+        System.out.print("This is an Elixir");
+    }
+}

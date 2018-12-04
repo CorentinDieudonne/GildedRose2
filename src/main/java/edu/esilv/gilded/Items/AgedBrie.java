a366090ad4c05785a44c4fe0cbddb2d19098d40c
JANIW Karl-Joris <kariiwar@gmail.com>
2018-12-04 18:59:34 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/edu/esilv/gilded/Items/Aged_Brie.java
    #	src/main/java/edu/esilv/gilded/Items/Concert.java
    #	src/main/java/edu/esilv/gilded/Items/Dexterity_Vest.java

--- src/main/java/edu/esilv/gilded/Items/AgedBrie.java
+++ src/main/java/edu/esilv/gilded/Items/AgedBrie.java
@@ -1,13 +1,76 @@
 package edu.esilv.gilded.Items;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class AgedBrie implements Strategy {
     private String name;
     private int quality;
     private int sellIn;
-    int id;
     LocalDateTime date;
+    List<AgedBrie> liste;
+
+    public AgedBrie(String name, int quality, int sellIn, int id, LocalDateTime date, List<AgedBrie> liste) {
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+        this.id = id;
+        this.date = date;
+        this.liste = liste;
+    }
+
+    int id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public List<AgedBrie> getListe() {
+        return liste;
+    }
+
+    public void setListe(List<AgedBrie> liste) {
+        this.liste = liste;
+    }
+
+
     AgedBrie(String name, int quality, int sellIn, int id, LocalDateTime date)
     {
         this.name=name;
--- src/main/java/edu/esilv/gilded/Items/Concert.java
+++ src/main/java/edu/esilv/gilded/Items/Concert.java
@@ -1,6 +1,7 @@
 package edu.esilv.gilded.Items;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Concert implements Strategy {
     private String name;
@@ -8,6 +9,68 @@ public class Concert implements Strategy {
     private int sellIn;
     int id;
     LocalDateTime date;
+    List<Concert> liste;
+    public Concert(String name, int quality, int sellIn, int id, LocalDateTime date, List<Concert> liste) {
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+        this.id = id;
+        this.date = date;
+        this.liste = liste;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public List<Concert> getListe() {
+        return liste;
+    }
+
+    public void setListe(List<Concert> liste) {
+        this.liste = liste;
+    }
+
+
+
     Concert(String name, int quality, int sellIn, int id, LocalDateTime date)
     {
         this.name=name;

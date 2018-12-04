2ba2656f04c7b2afaec6a55f5edd86131cad169a
Guillaume <guitiolord@hotmail.fr>
2018-12-04 17:42:17 +0100

    test

--- src/main/java/edu/esilv/gilded/PieChartSample.java
+++ src/main/java/edu/esilv/gilded/PieChartSample.java
@@ -12,19 +12,39 @@ public class PieChartSample extends Application {
 
     @Override public void start(Stage stage) {
         Scene scene = new Scene(new Group());
-        stage.setTitle("Imported Fruits");
+        stage.setTitle("Item repartition");
         stage.setWidth(500);
         stage.setHeight(500);
 
+        Inventory inventory = new Inventory();
+        int countBrie=0, countConcert=0, countVest=0, countElixir=0, countCake=0, countRagnaros=0;
+
+        for (int i = 0; i < inventory.getItems().length; i++) {
+            if (inventory.getItems()[i].getName().equals("Aged Brie")){countBrie++;}
+            if (inventory.getItems()[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")){countConcert++;}
+            if (inventory.getItems()[i].getName().equals("+5 Dexterity Vest")){countVest++;}
+            if (inventory.getItems()[i].getName().equals("Elixir of the Mongoose")){countElixir++;}
+            if (inventory.getItems()[i].getName().equals("Conjured Mana Cake")){countCake++;}
+            if (inventory.getItems()[i].getName().equals("Sulfuras, Hand of Ragnaros")){countRagnaros++;}
+        }
+
+
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
-                        new PieChart.Data("Grapefruit", 13),
-                        new PieChart.Data("Oranges", 25),
-                        new PieChart.Data("Plums", 10),
-                        new PieChart.Data("Pears", 22),
-                        new PieChart.Data("Apples", 30));
+                        new PieChart.Data("AgedBrie", countBrie),
+                        new PieChart.Data("Concert passes", countConcert),
+                        new PieChart.Data("Dexterity vest", countVest),
+                        new PieChart.Data("Elixir", countElixir),
+                        new PieChart.Data("ManaCake", countCake),
+                        new PieChart.Data("Ragnaros", countRagnaros));
+        /*new PieChart.Data(Integer.toString(countBrie), countBrie),
+                        new PieChart.Data(Integer.toString(countConcert), countConcert),
+                        new PieChart.Data(Integer.toString(countVest), countVest),
+                        new PieChart.Data(Integer.toString(countElixir), countElixir),
+                        new PieChart.Data(Integer.toString(countCake), countCake),
+                        new PieChart.Data(Integer.toString(countRagnaros), countRagnaros));*/
         final PieChart chart = new PieChart(pieChartData);
-        chart.setTitle("");
+        chart.setTitle("Item repartition");
 
         ((Group) scene.getRoot()).getChildren().add(chart);
         stage.setScene(scene);

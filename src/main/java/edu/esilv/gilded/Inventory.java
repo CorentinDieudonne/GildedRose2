022b03e6e1b3399efd797f8cde9235ff990eb525
JANIW Karl-Joris <kariiwar@gmail.com>
2018-12-11 16:22:36 +0100

    Merge branch 'master' of C:\Users\JANIW Karl-Joris\Documents\IBO A4\S7\Software developpement\GildedRose2 with conflicts.

--- src/main/java/edu/esilv/gilded/Inventory.java
+++ src/main/java/edu/esilv/gilded/Inventory.java
@@ -1,12 +1,78 @@
 package edu.esilv.gilded;
 
-import cucumber.api.java.it.Ma;
+import edu.esilv.gilded.Items.*;
 
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
 
 public class Inventory implements InventoryPart{
     public Item[] items;
+
+    public AgedBrie[] getABries() {
+        return ABries;
+    }
+
+    public void setABries(AgedBrie[] ABries) {
+        this.ABries = ABries;
+    }
+
+    public Concert[] getConcerts() {
+        return Concerts;
+    }
+
+    public void setConcerts(Concert[] concerts) {
+        Concerts = concerts;
+    }
+
+    public DexterityVest[] getDVests() {
+        return DVests;
+    }
+
+    public void setDVests(DexterityVest[] DVests) {
+        this.DVests = DVests;
+    }
+
+    public ManaCake[] getMCakes() {
+        return MCakes;
+    }
+
+    public void setMCakes(ManaCake[] MCakes) {
+        this.MCakes = MCakes;
+    }
+
+    public Ragnaros[] getHoRs() {
+        return HoRs;
+    }
+
+    public void setHoRs(Ragnaros[] hoRs) {
+        HoRs = hoRs;
+    }
+
+    public AgedBrie[] ABries=new AgedBrie[10];
+    public Concert[] Concerts=new Concert[10];
+
+    public Inventory(Item[] items, AgedBrie[] ABries, Concert[] concerts, DexterityVest[] DVests, Elixir[] elixirs, ManaCake[] MCakes, Ragnaros[] hoRs) {
+        this.items = items;
+        this.ABries = ABries;
+        Concerts = concerts;
+        this.DVests = DVests;
+        Elixirs = elixirs;
+        this.MCakes = MCakes;
+        HoRs = hoRs;
+    }
+
+    public DexterityVest[] DVests=new DexterityVest[10];
+    public Elixir [] Elixirs=new Elixir[10];
+    public ManaCake[] MCakes=new ManaCake[10];
+    public Ragnaros[] HoRs=new Ragnaros[10];
+
+    public Elixir[] getElixirs() {
+        return Elixirs;
+    }
+
+    public void setElixirs(Elixir[] elixirs) {
+        Elixirs = elixirs;
+    }
 
     public Inventory(Item[] items) {
         super();
@@ -28,9 +94,10 @@ public class Inventory implements InventoryPart{
                 new Item("Conjured Mana Cake", 3, 6,6,LocalDateTime.now()),
                 new Item("+5 Dexterity Vest", 10, 20,7, LocalDateTime.now())
         };
-    }
 
-    public void printInventory() {
+    };
+
+    private void printInventory() {
         System.out.println("***************");
         for (Item item : items) {
             System.out.println(item);
@@ -38,55 +105,178 @@ public class Inventory implements InventoryPart{
         System.out.println("***************");
         System.out.println("\n");
     }
-
-
-    public void itemlegacy()
-    {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].getName().equals("Aged Brie")){
-                AgedBrie brie = new AgedBrie(items[i].getName(),items[i].getSellIn(), items[i].getQuality(), items[i].getId(), items[i].getDate());
-                items[i]=brie;
-            }
-            if (items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")){
-                Concert concert = new Concert(items[i].getName(),items[i].getSellIn(), items[i].getQuality(), items[i].getId(), items[i].getDate());
-                items[i]=concert;
-            }
-            if (items[i].getName().equals("+5 Dexterity Vest")){
-                DexterityVest vest = new DexterityVest(items[i].getName(),items[i].getSellIn(), items[i].getQuality(), items[i].getId(), items[i].getDate());
-                items[i]=vest;
-            }
-            if (items[i].getName().equals("Elixir of the Mongoose")){
-                Elixir elixir = new Elixir(items[i].getName(),items[i].getSellIn(), items[i].getQuality(), items[i].getId(), items[i].getDate());
-                items[i]=elixir;
-            }
-            if (items[i].getName().equals("Conjured Mana Cake")){
-                ManaCake cake = new ManaCake(items[i].getName(),items[i].getSellIn(), items[i].getQuality(), items[i].getId(), items[i].getDate());
-                items[i]=cake;
-            }
-            if (items[i].getName().equals("Sulfuras, Hand of Ragnaros")){
-                Ragnaros ragnaros = new Ragnaros(items[i].getName(),items[i].getSellIn(), items[i].getQuality(), items[i].getId(), items[i].getDate());
-                items[i]=ragnaros;
-            }
+    public void updateQ(){
+        int j=0,j1=0,j2=0,j3=0,j4=0,j5=0;
+        while(j<ABries.length){
+            ABries[j].update();
+            j++;
+        }
+        while(j1<Concerts.length){
+            Concerts[j1].update();
+            j1++;
+        }
+        while(j2<DVests.length){
+            DVests[j2].update();
+            j2++;
+        }
+        while(j3<Elixirs.length){
+            Elixirs[j3].update();
+            j3++;
+        }
+        while(j4<MCakes.length){
+            MCakes[j4].update();
+            j4++;
+        }
+        while(j5<HoRs.length){
+            HoRs[j5].update();
+            j5++;
         }
     }
-
-
     public void updateQuality() {
-
         for (int i = 0; i < items.length; i++) {
-            items[i].update();
+            if (!items[i].getName().equals("Aged Brie")
+                    && !items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].getQuality() > 0) {
+                    if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                        items[i].setQuality(items[i].getQuality() - 1);
+                    }
+                }
+                if(items[i].getSellIn()<0){
+                    items[i].setQuality(items[i].getQuality()-2);
+                }
+            } else {
+                if (items[i].getQuality() < 50) {
+                    items[i].setQuality(items[i].getQuality() + 1);
+
+                    if (items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].getSellIn() < 11) {
+                            if (items[i].getQuality() < 50) {
+                                items[i].setQuality(items[i].getQuality() + 2);
+                            }
+                        }
+
+                        if (items[i].getSellIn() < 6) {
+                            if (items[i].getQuality() < 50) {
+                                items[i].setQuality(items[i].getQuality() + 3);
+                            }
+                        }
+                        if(items[i].getSellIn()<1){
+                            items[i].setQuality(0);
+                        }
+                    }
+                }
+            }
+            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros") && !items[i].getName().equals("Aged Brie")) {
+                items[i].setSellIn(items[i].getSellIn() - 1);
+            }
+
+            if (items[i].getSellIn() < 0) {
+                if (!items[i].getName().equals("Aged Brie")) {
+                    if (!items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].getQuality() > 0) {
+                            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                                items[i].setQuality(items[i].getQuality() - 1);
+                            }
+                        }
+                    } else {
+                        items[i].setQuality(0);
+                    }
+                } else {
+                    if (items[i].getQuality() < 50) {
+                        items[i].setQuality(items[i].getQuality() + 1);
+                    }
+                }
+            }
+            if("Aged Brie".equals(items[i].getName()) && items[i].getQuality()<50){
+                items[i].setSellIn(items[i].getSellIn()-1);
+            }
         }
     }
-
     public void accept(InventoryPartVisitor inventoryPartVisitor) { }
 
-
+    public Inventory FillClasses(){
+        Inventory inv=new Inventory();
+        int i=0; int k=items.length; int j=0,j1=0,j2=0,j3=0,j4=0,j5=0;
+        ArrayList<AgedBrie> abs=new ArrayList<AgedBrie>(); ArrayList<Concert> cs=new ArrayList<Concert>();
+        ArrayList<DexterityVest> dvs=new ArrayList<DexterityVest>(); ArrayList<Elixir> es=new ArrayList<Elixir>();
+        ArrayList<ManaCake> mcs=new ArrayList<ManaCake>(); ArrayList<Ragnaros> rs=new ArrayList<Ragnaros>();
+        while(i<k)
+        {
+            if(items[i].getName().equals("Aged Brie")){
+                AgedBrie a=new AgedBrie(null,0,0,0,null,null);
+                a.setDate(items[i].getDate());
+                a.setId(items[i].getId());
+                a.setQuality(items[i].getQuality());
+                a.setSellIn(items[i].getSellIn());
+                inv.ABries[j]=a;
+                abs.add(ABries[j]);
+                j++;
+            }
+            if(items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")){
+                Concert c=new Concert(null,0,0,0,null,null);
+                c.setDate(items[i].getDate());
+                c.setId(items[i].getId());
+                c.setQuality(items[i].getQuality());
+                c.setSellIn(items[i].getSellIn());
+                inv.Concerts[j1]=c;
+                cs.add(Concerts[j1]);
+                j1++;
+            }
+            if(items[i].getName().equals("+5 Dexterity Vest")){
+                DexterityVest d= new DexterityVest(null,0,0,0,null,null);
+                d.setDate(items[i].getDate());
+                d.setId(items[i].getId());
+                d.setQuality(items[i].getQuality());
+                d.setSellIn(items[i].getSellIn());
+                inv.DVests[j2]=d;
+                dvs.add(DVests[j2]);
+                j2++;
+            }
+            if(items[i].getName().equals("Elixir of the Mongoose")){
+                Elixir e=new Elixir(null,0,0,0,null,null);
+                e.setDate(items[i].getDate());
+                e.setId(items[i].getId());
+                e.setQuality(items[i].getQuality());
+                e.setSellIn(items[i].getSellIn());
+                inv.Elixirs[j3]=e;
+                es.add(Elixirs[j3]);
+                j3++;
+            }
+            if(items[i].getName().equals("Conjured Mana Cake")){
+                ManaCake m=new ManaCake(null,0,0,0,null,null);
+                m.setDate(items[i].getDate());
+                m.setId(items[i].getId());
+                m.setQuality(items[i].getQuality());
+                m.setSellIn(items[i].getSellIn());
+                inv.MCakes[j4]=m;
+                mcs.add(MCakes[j4]);
+                j4++;
+            }
+            if(items[i].getName().equals("Sulfuras, Hand of Ragnaros")){
+                Ragnaros r=new Ragnaros(null,0,0,0,null,null);
+                r.setDate(items[i].getDate());
+                r.setId(items[i].getId());
+                r.setQuality(items[i].getQuality());
+                r.setSellIn(items[i].getSellIn());
+                inv.HoRs[j5]=r;
+                rs.add(HoRs[j5]);
+                j5++;
+            }
+            i++;
+        }
+        inv.ABries[0].setListe(abs);
+        inv.Concerts[0].setListe(cs);
+        inv.DVests[0].setListe(dvs);
+        inv.Elixirs[0].setListe(es);
+        inv.MCakes[0].setListe(mcs);
+        inv.HoRs[0].setListe(rs);
+        return inv;
+    }
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
-        for (int i = 0; i < 11; i++) {
-            inventory.itemlegacy();
-            inventory.updateQuality();
-            inventory.printInventory();
-        }
+        inventory=inventory.FillClasses();
+        //inventory.updateQ();
+        System.out.println(inventory.ABries[0].getName());
+        inventory.printInventory();
     }
 }

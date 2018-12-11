eae23c03bc6d4a37a175995b830bd7f993d9c19f
CorentinDieudonne <corentin.dieudonne@gmail.com>
2018-12-11 14:44:37 +0100

    version 5

--- src/main/java/edu/esilv/gilded/json.java
+++ src/main/java/edu/esilv/gilded/json.java
@@ -3,22 +3,26 @@ package edu.esilv.gilded;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.swing.*;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
 public class json {
-
-    public static void main(String[] args) {
-
+    public static void ouvrirfichier()
+    {
+        final JFileChooser fc = new JFileChooser();
+    }
+    public static void jsonrecup(String fichier) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            FileReader file = new FileReader("jsn.txt");
-            List<Item> myObjects = mapper.readValue(file, new TypeReference<List<Item>>(){});
+            FileReader file = new FileReader(fichier);
+            List<Item> myObjects = mapper.readValue(file, new TypeReference<List<Item>>() {
+            });
             System.out.println("myObjects = " + myObjects);
         } catch (IOException e) {
             e.printStackTrace();
-        }
 
+        }
     }
 }
